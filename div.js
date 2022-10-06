2026c27a34d8229fcc6b4e54716c404ad2ccc1dd
S UNNIKRISHNAN <97491531+unnikrishnansk@users.noreply.github.com>
2022-10-06 16:44:10 +0530

    Add files via upload

---
+++ div.js
@@ -0,0 +1,5 @@
+const div = (a,b) => {
+    return a/b;
+}
+
+module.exports = div;
---
+++ index.js
@@ -0,0 +1,20 @@
+const add = require("./add");
+const sub = require("./sub");
+const mult = require("./mult");
+const div = require("./div");
+
+
+// const os = require("os");
+// console.log(os.cpus()[0]);
+
+// const fs = require("fs");
+// const data = fs.readFileSync("./add.js",{encoding : "utf-8"});
+// console.log(data);
+
+
+console.log(add(2,3));
+console.log(sub(5,2));
+console.log(mult(2,3));
+console.log(div(10,5));
+
+
